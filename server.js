b085b3c3a186c407867687cab70635a2ee39f807
aldoguardamino <aldo.guardamino@gmail.com>
2026-06-20 00:38:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -93,11 +93,14 @@ function enviarCorreo(to, subject, htmlBody) {
     try {
       const nodemailer = require('nodemailer');
       const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
         auth: {
           user: GMAIL_USER,
           pass: GMAIL_PASS.replace(/\s/g, '')
-        }
+        },
+        tls: { rejectUnauthorized: false }
       });
       transporter.sendMail({
         from: '"Sistema Akamai" <' + GMAIL_USER + '>',
